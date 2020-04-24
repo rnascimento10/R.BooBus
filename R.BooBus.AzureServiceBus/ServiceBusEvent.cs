8e3a59fa34a3feb4cec5335fae6afe9b5f3c67e6
Renato Teixeira do Nascimento <rnascimento10@outlook.com>
2020-04-24 14:59:23 -0300

    initial commit

---
+++ R.BooBus.AzureServiceBus/ServiceBusEvent.cs
@@ -0,0 +1,16 @@
+﻿using R.BooBus.Core;
+using System;
+
+namespace R.BooBus.AzureServiceBus
+{
+    public class ServiceBusEvent : IEvent
+    {
+
+        public ServiceBusEvent()
+        {
+
+        }
+        public Guid Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public DateTime CreateAt { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
